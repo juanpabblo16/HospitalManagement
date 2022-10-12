243cfe0220f78faee3cee7298952f793f1d8c3ac
Juan Pablo Acevedo Mira <juanpabblo789@gmail.com>
2022-10-12 09:54:59 -0500

    V2 - Added patient and doctor information, with UI and interactive methods that brings the final application expectations.

---
+++ src/hospital/Hospital.java
@@ -0,0 +1,14 @@
+
+package hospital;
+
+
+public class Hospital {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
